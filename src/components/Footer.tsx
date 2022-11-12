0c6cd24ae5344ecf66e6260d5d927c4b2a970997
Oleg Yadroshnikov <contact@olegyadroshnikov.com>
2022-11-12 01:43:28 -0600

    Add files via upload

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,7 @@ const Footer: React.FC = () => {
   return (
     <div className='footer'>
       <h3>
-        <span>©</span> myRP Tool, 2022. Personal practice web application for
-        personal use only.
+        <span>©</span> myRP Tool, 2022. Personal practice project only.
       </h3>
     </div>
   );
